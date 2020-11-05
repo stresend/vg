54c980d15d3fa0878757ebe10b9c4185af44bb19
Jordan Eizenga <joeizeng@gmail.com>
2020-11-05 13:06:41 -0800

    add draft implementation of a stack

--- src/vectorized_banded_aligner.cpp
+++ src/vectorized_banded_aligner.cpp
@@ -8,6 +8,134 @@
 #include "vectorized_banded_aligner.hpp"
 
 namespace vg {
+
+const size_t BandedVectorHeap::init_block_size = (1 << 20); // 1 MB
+
+BandedVectorAligner* BandedVectorAligner::init() {
     
+    BandedVectorHeap* heap = BandedVectorHeap::init();
+    BandedVectorAligner* aligner = (BandedVectorAligner*) heap->irreversible_alloc(sizeof(BandedVectorAligner));
+    aligner->heap = heap;
+    return aligner;
+}
+
+BandedVectorHeap* BandedVectorHeap::init() {
+    static_assert(init_block_size > sizeof(BandedVectorHeap), "heap must fit within the initial block");
+    BandedVectorMemoryBlock* init_block = BandedVectorMemoryBlock::init(init_block_size);
+    BandedVectorHeap* heap = (BandedVectorHeap*) init_block->irreversible_alloc(sizeof(BandedVectorHeap));
+    heap->head = heap->curr = init_block;
+    return heap;
+}
+
+void BandedVectorHeap::reset() {
+    BandedVectorMemoryBlock* prev = nullptr;
+    BandedVectorMemoryBlock* resetter = head;
+    while (prev != curr) {
+        resetter->reset();
+        prev = resetter;
+        resetter = resetter->next_block();
+    }
+    curr = head;
+}
+
+void* BandedVectorHeap::alloc(size_t _size) {
+    curr = curr->find_space(_size);
+    return curr->alloc(_size);
+}
+
+void* BandedVectorHeap::irreversible_alloc(size_t _size) {
+    curr = curr->find_space(_size);
+    return curr->irreversible_alloc(_size);
+}
+
+
+void BandedVectorHeap::destroy() {
+    
+    BandedVectorMemoryBlock* destroyer = head;
+    while (destroyer) {
+        BandedVectorMemoryBlock* next = destroyer->next_block();
+        destroyer->destroy();
+        destroyer = next;
+    }
+}
+
+BandedVectorHeap::BandedVectorMemoryBlock* BandedVectorHeap::BandedVectorMemoryBlock::init(size_t _size) {
+    
+    // add room for the block itself
+    size_t block_size = round_to_align(sizeof(BandedVectorMemoryBlock));
+    _size = round_to_align(_size) + block_size;
+    
+    void* mem;
+    int return_code = posix_memalign(&mem, sizeof(__m128i), _size);
+    if (return_code) {
+        cerr << "error: unable to posix_memalign " << _size << " bytes with alignment " << sizeof(__m128i) << endl;
+        exit(1);
+    }
+    
+    BandedVectorMemoryBlock* block = (BandedVectorMemoryBlock*) mem;
+    
+    // the pointer to be free'd
+    block->block_begin = (uint8_t*) mem;
+    // where the allocations can occur
+    block->bottom = block->curr = block->block_begin + block_size;
+    block->top = block->block_begin + _size;
+    
+    return block;
+}
+
+
+// from https://stackoverflow.com/questions/3407012/c-rounding-up-to-the-nearest-multiple-of-a-number
+size_t BandedVectorHeap::BandedVectorMemoryBlock::round_to_align(size_t _size) {
+    return (_size + sizeof(__m128i) - 1) & -sizeof(__m128i);
+}
+
+size_t BandedVectorHeap::BandedVectorMemoryBlock::remaining() const {
+    return top - curr;
+}
+
+size_t BandedVectorHeap::BandedVectorMemoryBlock::size() const {
+    return top - block_begin;
+}
+
+void* BandedVectorHeap::BandedVectorMemoryBlock::alloc(size_t _size) {
+    _size = round_to_align(_size);
+    void* ptr = curr;
+    curr += _size;
+    return ptr;
+}
+
+void* BandedVectorHeap::BandedVectorMemoryBlock::irreversible_alloc(size_t _size) {
+    void* ptr = alloc(_size);
+    bottom = curr;
+    return ptr;
+}
+
+void BandedVectorHeap::BandedVectorMemoryBlock::reset() {
+    curr = bottom;
+}
+
+BandedVectorHeap::BandedVectorMemoryBlock* BandedVectorHeap::BandedVectorMemoryBlock::find_space(size_t _size) {
+    BandedVectorMemoryBlock* prev = nullptr;
+    BandedVectorMemoryBlock* block = this;
+    while (block && block->remaining() < _size) {
+        prev = block;
+        block = block->next_block();
+    }
+    if (!block) {
+        block = init(max(_size, 2 * size()));
+        prev->next = block;
+    }
+    return block;
+}
+
+BandedVectorHeap::BandedVectorMemoryBlock* BandedVectorHeap::BandedVectorMemoryBlock::next_block() {
+    return next;
+}
+
+void BandedVectorHeap::BandedVectorMemoryBlock::destroy() {
+    free(block_begin);
+}
+
+
 
 }
--- src/vectorized_banded_aligner.hpp
+++ src/vectorized_banded_aligner.hpp
@@ -10,10 +10,106 @@
 #include<limits>
 #include<cstdint>
 #include<immintrin.h>
+#include<iostream>
 
 namespace vg {
 
 using namespace std;
+
+// forward declaration
+struct BandedVectorHeap;
+
+/*
+ * Core aligner object for vectorized banded global alignment
+ */
+struct BandedVectorAligner {
+    
+    
+    static BandedVectorAligner* init();
+    
+private:
+    
+    BandedVectorHeap* heap;
+    
+    
+public:
+    // delete all constructors, assignents operators, and the destructor
+    BandedVectorAligner() = delete;
+    ~BandedVectorAligner() = delete;
+    BandedVectorAligner(const BandedVectorAligner& other) = delete;
+    BandedVectorAligner(BandedVectorAligner&& other) = delete;
+    BandedVectorAligner& operator=(BandedVectorAligner&& other) = delete;
+    BandedVectorAligner& operator=(const BandedVectorAligner& other) = delete;
+};
+
+/*
+ * A heap that allocates 16-byte aligned memory pointers and allows
+ * collective freeing of many pointers simultaneously
+ */
+struct BandedVectorHeap {
+    
+public:
+    
+    // constructor
+    static BandedVectorHeap* init();
+    
+    // invalidate all existing pointers from this heap, and free up
+    // the memory to be reallocated
+    void reset();
+    
+    // allocate an aligned block of the size indicated.
+    void* alloc(size_t size);
+    
+    // allocate an aligned block of the size indicated, which will not
+    // be free'd when calling reset(). may make any allocation since the
+    // last time reset() was called permanant as well.
+    void* irreversible_alloc(size_t size);
+    
+    // destructor
+    void destroy();
+    
+private:
+    
+    static const size_t init_block_size;
+    
+    struct BandedVectorMemoryBlock {
+    public:
+        static BandedVectorMemoryBlock* init(size_t size);
+        
+        size_t remaining() const;
+        size_t size() const;
+        void* alloc(size_t size);
+        void* irreversible_alloc(size_t size);
+        void reset();
+        BandedVectorMemoryBlock* next_block();
+        BandedVectorMemoryBlock* find_space(size_t size);
+        
+        void destroy();
+        
+    private:
+        static size_t round_to_align(size_t size);
+        
+        uint8_t* block_begin;
+        uint8_t* bottom;
+        uint8_t* top;
+        uint8_t* curr;
+        BandedVectorMemoryBlock* next;
+    };
+    
+    BandedVectorMemoryBlock* head;
+    BandedVectorMemoryBlock* curr;
+    
+public:
+    // delete all constructors, assignents operators, and the destructor
+    BandedVectorHeap() = delete;
+    ~BandedVectorHeap() = delete;
+    BandedVectorHeap(const BandedVectorHeap& other) = delete;
+    BandedVectorHeap(BandedVectorHeap&& other) = delete;
+    BandedVectorHeap& operator=(BandedVectorHeap&& other) = delete;
+    BandedVectorHeap& operator=(const BandedVectorHeap& other) = delete;
+};
+
+
 
 struct SWVector {
     __m128i match;
@@ -22,36 +118,36 @@ struct SWVector {
 };
 
 struct BandedVectorMatrix {
-    
+
     // returns whether a coordinate in the full matrix is inside
     // the band
     bool is_inside_band(int i, int j);
-    
+
     // returns the index of the vector that contains a coordinate
     // from the full matrix
     int vector_index(int i, int j);
-    
+
     // returns the index within the vector conaining these
     // coordinates that corresponds to this exact coordinate
     int index_within_vector(int i, int j);
-    
+
     // returns coordinates in the full matrix that correspond
     // to an entry in a given vector
     pair<int, int> coordinate(int vec_idx, int idx_within_vec);
-    
+
     // returns the index of the vector that corresponds to the
     // entries directly above the indicated vector in the full matrix
     int vector_above(int vec_idx);
-    
+
     // returns the index of the vector that corresponds to the
     // entries directly below the indicated vector in the full matrix
     int vector_below(int vec_idx);
-    
+
     // returns the index of the vector that corresponds to the
     // entries up and to the left of the indicated vector in the
     // full matrix
     int vector_diagonal(int vec_idx);
-    
+
     SWVector* vectors;
     int64_t first_diag;
     int64_t num_diags;
